f6b1efd22ce5123a405a37fd8315ba4c800c4a7a
Navnath0006 <navnathkamble0007@gmail.com>
2023-02-23 12:28:24 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#Output");
-console.log(txtInput);
+
 
 function clickEventHandler() {
-    console.log("translate button clicked");
-    console.log("input : ", txtInput.value);
+    outputDiv.innerText = "translated message in banana :-> " + txtInput.value;
 }
 btnTranslate.addEventListener("click", clickEventHandler);
