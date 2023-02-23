7824f7f82a9eb6c35740da8f773dca2ce9b2c010
Navnath0006 <navnathkamble0007@gmail.com>
2023-02-23 15:28:22 +0530

    complete minion translator

--- app.js
+++ app.js
@@ -2,8 +2,29 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#Output");
 
+// var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var serverUrl = "	https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text) {
+    return serverUrl + "?" + "text=" + text
+}
+function errorHander(error) {
+    console.log("error occured ", error);
+    alert("Somthing wrong with servers!!! try after some time");
+
+}
 
 function clickEventHandler() {
-    outputDiv.innerText = "translated message in banana :-> " + txtInput.value;
+    // outputDiv.innerText = "translated message in banana :-> " + txtInput.value;
+    var inputText = txtInput.value;
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        //.then(json => console.log(json.contents.translated))
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHander);
 }
 btnTranslate.addEventListener("click", clickEventHandler);
